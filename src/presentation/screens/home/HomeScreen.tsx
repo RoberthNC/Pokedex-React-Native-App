9ac85c1573c22dcd3446bdb6b0e63b3d1d8afd88
Roberto Núñez <robertonc1599@gmail.com>
2024-06-10 22:01:43 -0500

    TanStackQuery and Mappers

--- src/presentation/screens/home/HomeScreen.tsx
+++ src/presentation/screens/home/HomeScreen.tsx
@@ -1,17 +1,29 @@
+import {useQuery} from '@tanstack/react-query';
 import {View} from 'react-native';
-import {Button, Text} from 'react-native-paper';
+import {ActivityIndicator, Button, Text} from 'react-native-paper';
+import {getPokemons} from '../../../actions/pokemons';
 
 export const HomeScreen = () => {
+  const {isLoading, data = []} = useQuery({
+    queryKey: ['pokemons'],
+    queryFn: () => getPokemons(0),
+    staleTime: 1000 * 60 * 60,
+  });
+
   return (
     <View>
       <Text variant="headlineLarge">HomeScreen</Text>
-      <Button
-        mode="contained"
-        onPress={() => {
-          console.log('Pressed');
-        }}>
-        Press me
-      </Button>
+      {isLoading ? (
+        <ActivityIndicator size="small" color="blue" />
+      ) : (
+        <Button
+          mode="contained"
+          onPress={() => {
+            console.log('Pressed');
+          }}>
+          Press me
+        </Button>
+      )}
     </View>
   );
 };
